abe056a1ae4a48ac066eb04a0ba3ad16f9927af7
codedreammer <nkanal38@gmail.com>
2024-07-22 19:34:18 +0530

    add the symmetric difference

--- set.py
+++ set.py
@@ -11,11 +11,25 @@ s1 = {1,2,5,6}
 s2 = {3,6,7}
 print(s1.union(s2))
 print(s1 , s2)
-'''
+
+#another example of union
 class1 = {"akshay","anand","ram","shyam","aam","nam"}
 class2 = {"kam","dham","akshay","alok","anand","dham"}
 classcombined = class1.union(class2)
 print(classcombined)
+
+#intersection (this gives the comman of all the list)
+class1 = {"akshay","anand","ram","shyam","aam","nam"}
+class2 = {"kam","dham","akshay","alok","anand","dham"}
+classcombined = class1.intersection(class2)
+print(classcombined)
+'''
+#symmetric_difference
+class1 = {"akshay","anand","ram","shyam","aam","nam"}
+class2 = {"kam","dham","akshay","alok","anand","dham"}
+classcombined = class1.symmetric_difference(class2)
+print(classcombined)
+
 
 
 
