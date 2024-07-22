1c2d01db92e953a9c5f970446891650e93ec3b87
codedreammer <nkanal38@gmail.com>
2024-07-22 19:25:46 +0530

    add the union example

--- set.py
+++ set.py
@@ -1,4 +1,4 @@
-s = {2,4,5,2}
+''' s = {2,4,5,2}
 print(s)
 
 info = {"Carla", 19, False, 5.9, 19}
@@ -11,5 +11,11 @@ s1 = {1,2,5,6}
 s2 = {3,6,7}
 print(s1.union(s2))
 print(s1 , s2)
+'''
+class1 = {"akshay","anand","ram","shyam","aam","nam"}
+class2 = {"kam","dham","akshay","alok","anand","dham"}
+classcombined = class1.union(class2)
+print(classcombined)
+
 
 
